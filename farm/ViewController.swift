733cecfbc4ce7760f007ad6bc01a84407e4ea85f
Anton Ogarkov <anton.ogarkov@sigma.software>
2020-06-30 08:36:02 +0300

    Initial Commit

---
+++ farm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  farm
+//
+//  Created by Anton Ogarkov on 30.06.2020.
+//  Copyright © 2020 SigmaSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
