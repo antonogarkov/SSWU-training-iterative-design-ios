ff0f639628a17d13d6849cf07815846299cac375
Anton Ogarkov <anton.ogarkov@sigma.software>
2020-07-22 10:10:45 +0300

    Create logo, add pods

---
+++ Podfile
@@ -0,0 +1,12 @@
+inhibit_all_warnings!
+use_frameworks!
+platform :ios, '12.0'
+workspace 'farm.xcworkspace'
+
+target 'farm' do
+   project 'farm.xcodeproj'
+
+   pod 'SkyFloatingLabelTextField', '3.7.0'
+   pod 'IQKeyboardManagerSwift', '6.5.5'
+
+end
---
+++ farm/UI/Helpers/NibInstantiatable.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/// Load ViewController from Storyboard
+protocol StoryboardInstantiatable: class {
+    /// Override the storyboardName if storyboard file has different name
+    static var storyboardName: String { get }
+}
+
+extension StoryboardInstantiatable {
+    static var storyboardName: String { return String(describing: self) }
+    static var bundleName: Bundle { return Bundle(for: self) }
+
+    static func instantiate() -> Self {
+        return instantiateWithName(name: storyboardName)
+    }
+
+    static func instantiateWithName(name: String) -> Self {
+        let storyboard = UIStoryboard(name: name, bundle: Bundle(for: self))
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as? Self
+            else { fatalError("failed to load \(name) storyboard file.") }
+        return viewController
+    }
+}
