cecd08c1c7f5c22f863ffe818a3d90b30076de8a
Anton Ogarkov <anton.ogarkov@sigma.software>
2020-08-04 23:35:37 +0300

    Use ContentSizedTableView for addresses list

---
+++ farm/UI/Helpers/ContentSizedTableView.swift
@@ -0,0 +1,15 @@
+// https://stackoverflow.com/questions/2595118/resizing-uitableview-to-fit-content
+import UIKit
+
+final class ContentSizedTableView: UITableView {
+    override var contentSize: CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
+    }
+}
